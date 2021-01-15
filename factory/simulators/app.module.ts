181614d68dbba171fec0702a5600861854b77124
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-01-15 16:26:26 +0100

    Simulator settings page

--- factory/simulators/app.module.ts
+++ factory/simulators/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule as ngRouterModule } from '@angular/router';
+import { RouterModule as ngRouterModule, Routes } from '@angular/router';
 import { CoreModule, BootstrapComponent, RouterModule, HOOK_NAVIGATOR_NODES } from '@c8y/ngx-components';
+import { SimulatorEntryComponent } from './src/simulator-entry/simulator-entry.component';
 import { NavFactory } from './factories/Navigation';
+import { CreateSimComponent } from './src/create-sim/create-sim.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SimulatorEntryComponent },
+  { path: 'createSim', component: CreateSimComponent },
+]
 
 @NgModule({
+  
   imports: [
     BrowserAnimationsModule,
     RouterModule.forRoot(),
-    ngRouterModule.forRoot([], { enableTracing: false, useHash: true }),
+    ngRouterModule.forRoot(appRoutes, { enableTracing: false, useHash: true }),
     CoreModule.forRoot()
   ],
-  providers: [
-    { provide: HOOK_NAVIGATOR_NODES, useClass: NavFactory, multi: true}
-  ],
+  declarations: [SimulatorEntryComponent, CreateSimComponent],
+  // entryComponents: [SimulatorEntryComponent],
+  // providers: [
+  //   { provide: HOOK_NAVIGATOR_NODES, useClass: NavFactory, multi: true}
+  // ],
   bootstrap: [BootstrapComponent]
 })
 export class AppModule {}
---
+++ factory/simulators/src/simulator-entry/simulator-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulator-entry',
+  templateUrl: './simulator-entry.component.html',
+  styleUrls: ['./simulator-entry.component.scss']
+})
+export class SimulatorEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
