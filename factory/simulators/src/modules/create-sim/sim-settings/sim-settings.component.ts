0a631e78466a5f9021cebccc520c18e555aa9997
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-01-29 10:23:50 +0100

    add events, alarms

--- factory/simulators/src/modules/create-sim/sim-settings/sim-settings.component.ts
+++ factory/simulators/src/modules/create-sim/sim-settings/sim-settings.component.ts
@@ -23,11 +23,55 @@ export class SimSettingsComponent implements OnInit {
     { category: "Location Update Device", code: "400" },
   ];
 
+  measurementOptions = [
+    "Measurement series one after another",
+    "Alternate measurement series",
+  ];
+  selectedMsmtOption = this.measurementOptions[0];
   selectedEventCategory = this.eventCategories[0].category;
+  measurements = [];
+  alarms: { level: string; type: string; text: string; steps: string;}[] = [];
+  events:
+     { level: string; type: string; text: string; steps: string; }[]
+    & {
+        level: string;
+        latitude: string;
+        longitude: string;
+        altitude: string;
+        accuracy: string;
+        steps: string;
+      }[] = [];
+
+  sleep: string;
+  fragment: string;
+  series: string;
+  minVal: string;
+  maxVal: string;
+  steps: string;
+  unit: string;
+
+  alarmType: string;
+  alarmText: string;
+  alarmSteps: string;
+
+  eventType: string;
+  eventText: string;
+
+  eventSteps: string;
+
+  latitude: string;
+  longitude: string;
+  altitude: string;
+  accuracy: string;
+
   ngOnInit() {}
 
   onChangeConfig(val) {
     this.selectedConfig = val;
+  }
+
+  onChangeMsmt(val) {
+    this.selectedMsmtOption = val;
   }
 
   onChangeOfAlarm(val) {
@@ -36,5 +80,70 @@ export class SimSettingsComponent implements OnInit {
 
   onChangeEvent(val) {
     this.selectedEventCategory = val;
+  }
+
+  addMsmtToArray() {
+    this.measurements.push({
+      fragment: this.fragment ? this.fragment : "",
+      series: this.series ? this.series : "",
+      minValue: this.minVal ? this.minVal : "",
+      maxVal: this.maxVal ? this.maxVal : "",
+      steps: this.steps ? this.steps : "",
+      unit: this.unit ? this.unit : "",
+      sleep: this.sleep ? this.sleep : "",
+    });
+    this.fragment = "";
+    this.maxVal = "";
+    this.minVal = "";
+    this.series = "";
+    this.steps = "";
+    this.unit = "";
+  }
+
+  addAlarmToArray() {
+    const level = this.alarmCategories.find(
+      (entry) => entry.category === this.selectedAlarmCategory
+    ).code;
+    this.alarms.push({
+      level: level,
+      type: this.alarmType,
+      text: this.alarmText,
+      steps: this.alarmSteps,
+    });
+    this.alarmText = "";
+    this.alarmType = "";
+    this.alarmSteps = "";
+  }
+
+  addEventToArray() {
+    switch (this.selectedEventCategory) {
+      case this.eventCategories[0].category:
+        this.events.push({
+          level: this.eventCategories[0].code,
+          type: this.eventType,
+          text: this.eventText,
+          steps: this.eventSteps
+        });
+        this.eventText = "";
+        this.eventType = "";
+        break;
+
+      case this.eventCategories[1].category || this.eventCategories[2].category:
+        this.events.push({
+          level: this.eventCategories.find(
+            (temp) => temp.category === this.selectedEventCategory
+          ).code,
+          latitude: this.latitude,
+          longitude: this.longitude,
+          altitude: this.altitude,
+          accuracy: this.accuracy,
+          steps: this.eventSteps
+        });
+        this.latitude = "";
+        this.longitude = "";
+        this.altitude = "";
+        this.accuracy = "";
+        break;
+    }
   }
 }
