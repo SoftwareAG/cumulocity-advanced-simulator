119dccf6bfccd349c69b83a07d9ba705b2c11a67
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-01-29 08:13:07 +0100

    Layouts for sleep, alarm and events

--- factory/simulators/src/modules/create-sim/sim-settings/sim-settings.component.ts
+++ factory/simulators/src/modules/create-sim/sim-settings/sim-settings.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sim-settings',
-  templateUrl: './sim-settings.component.html',
-  styleUrls: ['./sim-settings.component.scss']
+  selector: "app-sim-settings",
+  templateUrl: "./sim-settings.component.html",
+  styleUrls: ["./sim-settings.component.scss"],
 })
 export class SimSettingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  
-  defaultConfig: string[] = ['Measurements', 'Alarms', 'Events', 'Sleep'];
+  defaultConfig: string[] = ["Measurements", "Alarms", "Events", "Sleep"];
+  alarmCategories = [
+    { category: "Critical", code: "301" },
+    { category: "Major", code: "302" },
+    { category: "Minor", code: "303" },
+  ];
   selectedConfig: string = this.defaultConfig[0];
-  ngOnInit() {
-  }
+  selectedAlarmCategory = this.alarmCategories[0].category;
+
+  eventCategories = [
+    { category: "Basic", code: "400" },
+    { category: "Location Update", code: "400" },
+    { category: "Location Update Device", code: "400" },
+  ];
+
+  selectedEventCategory = this.eventCategories[0].category;
+  ngOnInit() {}
 
   onChangeConfig(val) {
     this.selectedConfig = val;
   }
 
+  onChangeOfAlarm(val) {
+    this.selectedAlarmCategory = val;
+  }
+
+  onChangeEvent(val) {
+    this.selectedEventCategory = val;
+  }
 }
