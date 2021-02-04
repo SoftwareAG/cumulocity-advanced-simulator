baea9dedb022c7e547c5ae1f1b2ee8cf4898b6b2
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-02-04 07:23:04 +0100

    measurements and alarms component

---
+++ factory/simulators/src/modules/create-sim/sim-settings/sim-alarms/sim-alarms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sim-alarms',
+  templateUrl: './sim-alarms.component.html',
+  styleUrls: ['./sim-alarms.component.scss']
+})
+export class SimAlarmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ factory/simulators/src/modules/create-sim/sim-settings/sim-measurements/sim-measurements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sim-measurements',
+  templateUrl: './sim-measurements.component.html',
+  styleUrls: ['./sim-measurements.component.scss']
+})
+export class SimMeasurementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
