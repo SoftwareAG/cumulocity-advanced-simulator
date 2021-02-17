7f582629b940b7b0c4ab512eb794194df3b60caa
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-02-17 08:46:18 +0100

    tab for supported operations

---
+++ factory/simulators/src/modules/supported-operations/supported-operations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supported-operations',
+  templateUrl: './supported-operations.component.html',
+  styleUrls: ['./supported-operations.component.less']
+})
+export class SupportedOperationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
