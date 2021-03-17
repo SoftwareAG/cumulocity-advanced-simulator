403a30a0924caeb2cf255e5a5482ecb698b84c51
Deepthi Deepthi (ddee) <Deepthi.Deepthi@softwareag.com>
2021-03-17 17:24:10 +0100

    minor changes

--- factory/simulators/src/models/inputFields.const.ts
+++ factory/simulators/src/models/inputFields.const.ts
@@ -33,7 +33,7 @@ export const MeasurementsForm: InputField[] = [
   {
     name: "Randomize",
     required: false,
-    label: "Randomize",
+    label: "Randomize Measurements",
     type: 'switch'
   },
   {
