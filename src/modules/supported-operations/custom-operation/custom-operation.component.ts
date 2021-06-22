e55f38e0c5179d47305dc04f297723e27976f0da
Dirk Peter <dirk.peter@softwareag.com>
2021-06-22 12:32:13 +0200

    #49: moves model and template to sepatare file

--- src/modules/supported-operations/custom-operation/custom-operation.component.ts
+++ src/modules/supported-operations/custom-operation/custom-operation.component.ts
@@ -1,64 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { InventoryService } from "@c8y/ngx-components/api";
-import { Subject } from "rxjs";
-import { IManagedObject } from "@c8y/client";
-import {
-  CustomSimulator,
-  DeviceSimulator,
-  SimulatorModel,
-} from "src/models/simulator.model";
-import { Router } from "@angular/router";
+import { Component, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CustomSimulator } from 'src/models/simulator.model';
 import { SimulatorsServiceService } from '@services/simulatorsService.service';
-export interface ILabels {
-  ok?: string;
-  cancel?: string;
-}
-
+import { ILabels } from '@models/labels.model';
 
 @Component({
-  selector: "addCustomOperation",
-  template: ` <c8y-modal
-    title="Add Custom Operation"
-    (onClose)="saveCustomOperation($event)"
-    (onDismiss)="onDismiss($event)"
-    [labels]="labels"
-  >
-    <ng-form>
-      <div class="inputs">
-        <div class="form-group width-33">
-          <label translate>Custom Operation Type *</label>
-          <input
-            class="form-control"
-            [(ngModel)]="customOperationTitle"
-            placeholder="e.g. myCustomOperation (required)"
-            [ngModelOptions]="{ standalone: true }"
-          />
-        </div>
-      </div>
-    </ng-form>
-  </c8y-modal>`,
+  selector: 'addCustomOperation',
+  styleUrls: ['./custom-operation.component.scss'],
+  templateUrl: './custom-operation.component.html'
 })
-export class CustomOperationComponent implements OnInit {
+export class CustomOperationComponent {
   private closeSubject: Subject<any> = new Subject();
   customOperationTitle: string;
   public labels: ILabels = {
-    ok: "Save",
-    cancel: "Cancel",
+    ok: 'Save',
+    cancel: 'Cancel'
   };
   @Input() mo: CustomSimulator;
-  constructor(private simulatorService: SimulatorsServiceService) { }
 
-  ngOnInit() {
-  }
+  constructor(private simulatorService: SimulatorsServiceService) {}
 
   saveCustomOperation() {
-    if (!this.mo.c8y_DeviceSimulator.hasOwnProperty("c8y_CustomOperations")) {
-      this.mo.c8y_DeviceSimulator["c8y_CustomOperations"] = [];
+    if (!this.mo.c8y_DeviceSimulator.hasOwnProperty('c8y_CustomOperations')) {
+      this.mo.c8y_DeviceSimulator['c8y_CustomOperations'] = [];
     }
     this.mo.c8y_DeviceSimulator.c8y_SupportedOperations.push(this.customOperationTitle);
     this.mo.c8y_DeviceSimulator.c8y_CustomOperations.push(this.customOperationTitle);
     this.simulatorService.updateSimulatorManagedObject(this.mo).then((res) => {}); // FIXME proper handling
-
   }
 
   onDismiss(event) {
@@ -68,5 +36,4 @@ export class CustomOperationComponent implements OnInit {
   onClose(event) {
     this.closeSubject.next(event);
   }
-
 }
